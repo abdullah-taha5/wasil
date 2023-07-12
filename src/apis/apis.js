1834bb1af64e42663bdaf3877e6a663a33719b0c
abdullah-taha5 <75340111+abdullah-taha5@users.noreply.github.com>
2023-07-12 16:59:21 +0300

    Create apis.js

---
+++ src/apis/apis.js
@@ -0,0 +1,27 @@
+// * Base Url API * //
+const BASE_URL = "https://wasil.stackdeans.xyz/api/v10";
+// * Src Image API * //
+export const SRC_IMG_API = "https://wasil.stackdeans.xyz/public";
+// * Endpoints API * //
+export const CITIES_API = `${BASE_URL}/get_cities`;
+export const REGISTER_API = `${BASE_URL}/register`;
+export const LOGIN_API = `${BASE_URL}/signin`;
+export const LOGOUT_API = `${BASE_URL}/sign-out`;
+export const CALCULATOR_API = `${BASE_URL}/calculator`;
+export const MEMBERSHIPS_API = `${BASE_URL}/landing_index`;
+export const USER_DASHBOARD_API = `${BASE_URL}/user_dashboard`;
+export const UPDATE_CURRENT_CODE_API = `${BASE_URL}/update_current_code`;
+export const INBOX_API = `${BASE_URL}/get_on_box_packs`;
+export const GET_PACKAGE_API = `${BASE_URL}/get_package`;
+export const SEARCH_PACKAGES_API = `${BASE_URL}/search_my_packages`;
+export const EDIT_PRICE_API = `${BASE_URL}/send_edit_price_request`;
+export const SAVE_DV_API = `${BASE_URL}/save_dv_value`;
+export const UPLOAD_MERCHANT_INVOICE_API = `${BASE_URL}/upload_merchant_invoice`;
+export const RETURN_PACKAGE_API = `${BASE_URL}/return_package`;
+export const GET_RETURN_PRICE_API = `${BASE_URL}/getReturnPrice`;
+export const MY_MEMBERSHIPS_API = `${BASE_URL}/user/get-my-memberships`;
+export const GET_PLANS_API = `${BASE_URL}/plans/byHubId`;
+export const SUBSCRIBE_API = `${BASE_URL}/wasil-subscribe`;
+export const PROMO_CODE_API = `${BASE_URL}/promocode/save`;
+export const REMOVE_PROMO_CODE_API = `${BASE_URL}/promocode/remove`;
+export const OUTBOX_API = `${BASE_URL}/get_out_box_packs`;
